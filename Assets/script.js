d7aa48a0bd12518bb0db8ca09a7d45839b937549
Victor Tran <victortran415@gmail.com>
2020-11-06 16:46:48 -0800

    cant get searched city and UV to appear

--- Assets/script.js
+++ Assets/script.js
@@ -8,7 +8,7 @@ var clearBtn = $("#clear-list")
 var cityInput = $("#search-city");
 
 var cityName = $(".cityName");
-var historyList = $(".list-group")
+var historyList = $(".history-list")
 var icon = $("#icon")
 
 var tempEle = $("#temp");
@@ -17,32 +17,34 @@ var windSpeedEle = $("#windspeed")
 var uvIdxEle = $("#uv-idx")
 
 
-
 searchBtn.on("click", function(e) {
     e.preventDefault();
-    // FIXME: not operating right, may delete 
-    if (cityInput.val() === undefined) {
-        alert("City name not found");
-        return;
-    }
-    // checking to see if button operating correctly
     console.log('clicked')
     currentWeather(cityInput.val())
 })
 
-//button to clear searched history
-clearBtn.on("click", function(){
-    console.log("history list")
-})
 
-function weatherData(name, temp, humidity, windSpd, iconID) {
+
+function cityHistory(name){
+    historyList.empty();
+    var cityArr = JSON.parse(localStorage.getItem("searchCity"));
+    for (var i = 0; i < cityArr.length; i++) {
+        let cityList = $("<li>").attr("class", newEntry);
+        cityList.text(cityArr[i])
+        cityList.append.historyList
+    }
+}
+
+
+function weatherData(name, temp, humidity, windSpd, iconID, uvIdx) {
     cityName.text(name);
     tempEle.text(" " + temp + "°F");
     humidityEle.text(" " + humidity + "%");
     windSpeedEle.text(" " + windSpd + " MPH")
     icon.attr("src", iconID)
+    uvIdxEle.text(" " + uvIdx)
 }
-console.log(weatherData)
+
 
 function currentWeather(searchedCity) {
     var weatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&APPID=e0876bb83e7f5e06b1c6b43fc115403c&units=imperial`;
@@ -56,18 +58,22 @@ function currentWeather(searchedCity) {
             temp: currWeatherInfo.main.temp,
             humidity: currWeatherInfo.main.humidity,
             windSpd: currWeatherInfo.wind.speed,
-            iconID: currWeatherInfo.weather[0].icon
+            iconID: currWeatherInfo.weather[0].icon,
+            uvIdx: currWeatherInfo.coord
             // UPDATE: need to add an UV Index
         }
 
+        var uvUrl = `https://api.openweathermap.org/data/2.5/uvi?lat=${cityInfo.uvIdx.lat}&lon=${cityInfo.uvIdx.lon}&APPID=e0876bb83e7f5e06b1c6b43fc115403c&units=imperial`
+    $.ajax({
+        url: uvUrl,
+        method: 'GET'
+    })
         // created an icon for the current weather
-        var iconID = `https:///openweathermap.org/img/w/${cityInfo.iconID}.png`
+        var iconIDurl = `https:///openweathermap.org/img/w/${cityInfo.iconID}.png`
         weatherData(
-        cityInfo.name,cityInfo.temp,cityInfo.humidity, cityInfo.windSpd, iconID)
+        cityInfo.name,cityInfo.temp,cityInfo.humidity, cityInfo.windSpd, iconIDurl)
 
         // UPDATE: must add UV info above ^^^^:
 
     })
 }
-
-
